fb94003308dd23a746689e9a114b6c5ac6114bd8
Abhay Shivhare <abhay.shivhare@xomic.local>
2024-05-14 18:37:33 +0530

    14-05-2024

--- src/ArraysSearching/MoveAllZeroesToEndOfArray.java
+++ src/ArraysSearching/MoveAllZeroesToEndOfArray.java
@@ -11,8 +11,86 @@ package ArraysSearching;
 public class MoveAllZeroesToEndOfArray {
 
 	public static void main(String[] args) {
+		int arr[] = {1, 2, 0, 4, 3, 0, 5, 0};
+	    int 	arr2[]  = {1, 2, 0, 0, 0, 3, 6};
+		//moveAllZeroesToEndOfArray(arr);
+		//moveAllZeroesToEndOfArray(arr2);
+		moveAllZeroesToEndOfArrayUsingSingleLoop(arr);
+		//moveAllZeroesToEndOfArrayUsingSingleLoop(arr2);
+		
 		
 
 	}
+	//using two loop
+	static void moveAllZeroesToEndOfArray(int arr[])
+	{
+		
+	
+		int temp[]= new int[arr.length];
+		int count=0;
+		int k=0;
+		for(int i=0;i<arr.length; i++)
+		{
+			if(arr[i]!=0)
+			{
+				temp[k]=arr[i];
+				k++;
+				count++;
+			}
+			
+		}
+		
+		System.out.println("temp array elements");
+		for(int i:temp)
+		{
+			
+			System.out.println(i);
+		}
+		System.out.println("Count :"+count);
+		System.out.println("K  :"+k);
+		for(int i =count; i<temp.length; i++ )
+		{
+			temp[k]=0;
+			k++;
+		}
+		System.out.println("After copying zero to new temp array");
+		System.out.println("temp array elements");
+		for(int i:temp)
+		{
+			
+			System.out.println(i);
+		}
+		System.out.println("Count :"+count);
+		System.out.println("K  :"+k);
+		
+		
+		
+	}
+	
+	
+	//Using single loop
+	static void moveAllZeroesToEndOfArrayUsingSingleLoop(int arr[])
+	{
+		int count=0;
+		for(int i=0; i<arr.length;i++)
+		{
+			if(arr[i]!=0)
+			{
+				arr[count]= arr[i];
+				count++;
+			}
+			
+		}
+		while(count<arr.length)
+		{
+			arr[count]=0;
+			count++;
+		}
+		
+		for(int i: arr)
+		System.out.println("array elements" + i);
+		
+	}
+	
 
 }
---
+++ src/Strings/DemoSpring.java
@@ -0,0 +1,17 @@
+package Strings;
+
+public class DemoSpring {
+
+	public static void main(String[] args) {
+		String s= "abhay";
+		s= s+ "fgshsh";
+		//s.concat("dfff");
+		System.out.println(s);
+		
+		String demoString = new String("GeeksforGeeks");
+		String concat = demoString.concat("hello");
+		System.out.println(concat);
+
+	}
+
+}
